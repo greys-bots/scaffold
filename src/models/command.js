64cb953a2eb720e227fdf52dd0d909c79b10fbae
greysdawn <greysdawn@gmail.com>
2022-06-07 13:39:47 -0400

    Initial

---
+++ src/models/command.js
@@ -0,0 +1,49 @@
+const DKEYS = [
+	'name',
+	'description'
+	'type',
+	'options'
+]
+
+class SlashCommand {
+	name;
+	description;
+	type = 1;
+	options = [];
+
+	usage = [];
+	guildOnly = false;
+	permissions = [];
+	ephemeral = false;
+	
+	constructor(data) {
+		for(var k in data) this[k] = data[k];
+	}
+
+	async execute(ctx) {
+		return "Override this!";
+	}
+
+	async auto(ctx) {
+		return [{
+			name: 'Override this!',
+			value: 'unset'
+		}]
+	}
+
+	transform() {
+		var data = { };
+		for(var k of DKEYS) {
+			data[k] = this[k]
+		}
+
+		if(this.guildOnly)
+			data.dm_permission = false;
+		if(this.permissions?.length)
+			data.default_member_permissions = '0';
+
+		return data;
+	}
+}
+
+module.exports = { SlashCommand }
