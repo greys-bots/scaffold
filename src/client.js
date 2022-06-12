7f29217b10cb84080bfbb1c907617f7fbe28e7fc
greysdawn <greysdawn@gmail.com>
2022-06-12 14:00:40 -0400

    Bot properties and status handling

--- src/client.js
+++ src/client.js
@@ -1,8 +1,26 @@
 const { Client, Intents } = require('discord.js');
 
 class FrameClient extends Client {
-	constructor(data) {
-		super(data);
+	constructor(clientOptions, botData) {
+		super(clientOptions);
+
+		for(var k in botData) this[k] = botData[k];
+		this.status = 0;
+
+		if(this.statuses?.length) {
+			this.on('ready', () => this.handleStatus());
+		}
+	}
+
+	async handleStatus() {
+		if(!this.statuses?.length) return;
+
+		var target = this.statuses[this.status % this.statuses.length];
+		if(typeof target == "function") this.user.setActivity(await target());
+		else this.user.setActivity(target);
+		this.status++;
+			
+		setTimeout(()=> this.handleStatus(), 60 * 1000) // 5 mins
 	}
 }
 
